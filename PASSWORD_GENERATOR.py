4a2e1763b503f0995ba039861a3880e5c7749f36
In Dev <starlightgames.g@gmail.com>
2020-09-06 12:30:41 +0530

    Update PASSWORD_GENERATOR.py

--- PASSWORD_GENERATOR.py
+++ PASSWORD_GENERATOR.py
@@ -4,9 +4,10 @@ small = "abcdefghijklmnopqrstuvwxyz"
 big = "ABCDEFGHIJKLMNOPQRSTUVWXYS"
 num = "123456789"
 symbols = "@,.-_"
-
+#hdhhsgkjsgfjgjdsgfgjsg
 all = symbols+num+small
 lenth = 8
 passs = "".join(random.sample(all,lenth))
 
+#svhgsvhdgjhdhsdhhfdjfjds
 print(passs)
