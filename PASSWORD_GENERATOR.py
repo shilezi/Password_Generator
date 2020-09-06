ff849541c62ecc90aa361cd7e406ebf582b153ed
IndieD3v <starlightgames.g@gmail.com>
2020-09-06 12:34:37 +0530

    cd

--- PASSWORD_GENERATOR.py
+++ PASSWORD_GENERATOR.py
@@ -4,14 +4,9 @@ small = "abcdefghijklmnopqrstuvwxyz"
 big = "ABCDEFGHIJKLMNOPQRSTUVWXYS"
 num = "123456789"
 symbols = "@,.-_"
-#hdhhsgkjsgfjgjdsgfgjsg
+
 all = symbols+num+small
 lenth = 8
 passs = "".join(random.sample(all,lenth))
 
-#svhgsvhdgjhdhsdhhfdjfjds
 print(passs)
-dsahdvhghdadhggh
-sdvfhhsd
-vsdghjdsj
-sdgjfgjdsgjfgjdsgggcfsdgjdg
