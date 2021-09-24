82dc32e34d20bb41ac982700536ad0584da03d75
shilezi <shilezi@hotmail.com>
2021-09-24 15:39:36 -0400

    Update PASSWORD_GENERATOR.py

--- PASSWORD_GENERATOR.py
+++ PASSWORD_GENERATOR.py
@@ -4,10 +4,10 @@ import random
 small = "abcdefghijklmnopqrstuvwxyz"
 big = "ABCDEFGHIJKLMNOPQRSTUVWXYS"
 num = "123456789"
-symbols = "@,.-_"
+symbols = "!@#$%^&()_+<>?":{}|[]"
 
 all = symbols+num+small
-lenth = 8
+lenth = 12
 passs = "".join(random.sample(all,lenth))
 
 print(passs)
